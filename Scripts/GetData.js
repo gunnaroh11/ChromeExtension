a1ede4f1989ed57c6dc2062696f082088e8df1b3
gunnarha <gunnaroh11@gmail.com>
2014-06-08 23:49:38 +0000

    html css og manifest baett vid

--- Scripts/GetData.js
+++ Scripts/GetData.js
@@ -1,34 +1,22 @@
-function getallevents(){
 
-var xmlhttp;
-if (window.XMLHttpRequest)
-  {// code for IE7+, Firefox, Chrome, Opera, Safari
-  xmlhttp=new XMLHttpRequest();
+
+(function() {
+  if (! window.jQuery ) {
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = '//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js'; // you can change this url by latest jQuery version
+    (document.getElementsByTagName('head')[0] ||
+      document.getElementsByTagName('body')[0]).appendChild(s);
   }
-else
-  {// code for IE6, IE5
-  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-
-  	var Url = "inpersoned.com/Activity/Xml.aspx";
-  	xmlHttp.onreadystatechange = ProcessRequest;
-	xmlhttp.open("GET",Url,true);
-	xmlhttp.send(null);
-	
-}
-
-function ProcessRequest () {
-	// body...
-}
-
+}());
 
 var xmlHttp = null;
 
-function GetCustomerInfo()
-{
-    window.location.href = "http://www.verkskraning.is";//"http://inpersoned.com/Activity/Xml.aspx";
-    //var Url = "inpersoned.com/Activity/Xml.aspx";
-    var Url = "http://www.verkskraning.is";
+function GetAllEvents()
+{    
+    var Url = "inpersoned.com/Activity/Xml.aspx";
+    //var Url = "http://www.verkskraning.is";
     xmlHttp = new XMLHttpRequest(); 
     xmlHttp.onreadystatechange = ProcessRequest;
     xmlHttp.open( "GET", Url, true );
@@ -45,12 +33,32 @@ function ProcessRequest()
         }
         else
         {
-        	
+        	alert('woohoo found');
+        	var elemContainer = document.getElementById("ActivityList");
+        	//elemContainer.innerHTML=xmlhttp.responseText;
             //var info = eval ( "(" + xmlHttp.responseText + ")" );
-            var info = xmlHttp.responseText;
-            // No parsing necessary with JSON!  
-            console.log(info);      
-            
+            var info = elemContainer.innerHTML;
+            xmlParser(info);
+            alert(info);
         }                    
     }
+}
+
+function xmlParser(xml) {
+
+var activityList = $(xml).find("ActivityList");
+var eventNameList = $(xml).find("EventName");
+activityList.each(function () {
+	
+	var eventcolstr = "";
+	eventcolstr += $(this).find("EventName").text();
+	eventcolstr += $(this).find("StartDate").text();
+	eventcolstr += $(this).find("Location").text();
+	eventcolstr += $(this).find("TargetGender").text();
+	eventcolstr += $(this).find("CreatorName").text();
+	alert(eventcolstr);
+    //$(".main").append('<div class="book"><div class="title">' + $(this).find("Title").text() +   '</div><div class="description">' + $(this).find("Description").text() + '</div><div   class="date">Published ' + $(this).find("Date").text() + '</div></div>');
+    //$(".book").fadeIn(1000);
+ });
+
 }
